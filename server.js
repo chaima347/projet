d497e53608a2b4806967a5217e8a4339a2135870
chaima <shouikhsheimaa@gmail.com>
2024-11-12 14:02:28 +0100

    final configuration

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.error("MongoDB connection error:", err));
 
+
 // Basic route
 app.get('/', (req, res) => res.send('Election Management App Backend'));
 
